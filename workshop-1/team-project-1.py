b77945ca57a0429689566bb474f9a05d8af5f134
qz-fordham <45869139+qz-fordham@users.noreply.github.com>
2019-01-24 23:50:35 -0500

    merge develop (#3)
    * upload team project part 1

---
+++ workshop-1/team-project-1.py
@@ -0,0 +1,43 @@
+# Don't modify directly on this file!
+
+# TODO: Finish following 3 questions. Everyone in team should work on at least one question. It's ok to have multiple solutions for same question (for team with members more than 3).
+
+# Acceptable Condition (AC):
+#   - A repo on GitHub with 3 solution files and this file.
+#   - File name should be function_name.py (e.g. is_anagram.py)
+#   - If there are multiple solution for a question, name file as function_name_1.py (e.g. is_anagram_1.py)
+
+
+# Question 1
+# When read string 's' backward, if it's same as read it forward, then we say 's' is a palindrome. e.g. "racecar" reading backward is still "racecar".
+# Assume letters in 's' will always be lower case.
+# Return if 's' is a palindrome
+def is_palindrome(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    pass
+
+
+# Question 2
+# By re-arrange letter in 's', if we can get 't', then we say 's' & 't' is anagram to each other. e.g. "listen" and "silent" are anagrams to each other.
+# Assume letters in 's' & 't' will always be lower case.
+# Return if 's' & 't' are anagrams to each other
+def is_anagram(s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    pass
+
+
+# Question 3
+# Return if 'num' is a prime number
+def is_prime(num):
+    """
+    :type num: int
+    :rtype: bool
+    """
+    pass
